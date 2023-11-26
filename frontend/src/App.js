8da5837f5859c632f93036619ec5b685b5af4e8e
biekeb <91184065+biekeb@users.noreply.github.com>
2023-11-26 23:39:17 +0100

    Feat: Creating routes for the page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import Login from './Login'
+import React from 'react';
+import Login from './components/Login';
 
 const App = () => {
   return (
     <div>
-      
       <Login />
       
     </div>
-  )
-}
 
-export default App
+  );
+};
+
+export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,15 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter, Routes, Route } from 'react-router-dom';
+import AdminDashboards from './components/AdminDashboards';
+import UserDashboard from './components/UserDashboard';
+import Signup from './components/Signup';
+import ProtectedRoute from './components/ProtectedRoute'; // Adjust the import path
+import "./main.css"
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/admin',
+    element: <ProtectedRoute element={<AdminDashboards />} allowedRoles={['admin']} />,
+  },
+  {
+    path: '/user',
+    element: <ProtectedRoute element={<UserDashboard />} allowedRoles={['user']} />,
+  },
+  {
+    path: '/signup',
+    element: <Signup />,
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/admin" element={<AdminDashboards />} />
+        <Route path="/user" element={<UserDashboard />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </RouterProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
